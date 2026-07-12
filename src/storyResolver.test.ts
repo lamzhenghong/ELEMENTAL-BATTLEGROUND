14907637221681fb69fc582a9100176ea6876138
lamzhenghong <lamzhenghong@gmail.com>
2026-07-13 03:33:56 +0800

    Update resolver transition coverage

--- src/storyResolver.test.ts
+++ src/storyResolver.test.ts
@@ -1,6 +1,7 @@
 import assert from 'node:assert/strict';
 import { getStageSpec, getStoryModifier } from './data/story';
 
-assert.throws(() => getStageSpec('4-1'), /authored story stage/i);
+assert.equal(getStageSpec('4-1').name, 'Gloamvault Descent');
+assert.throws(() => getStageSpec('6-1'), /authored story stage/i);
 assert.equal(getStoryModifier('1-1'), undefined);
 console.log('story resolver foundation ok');
